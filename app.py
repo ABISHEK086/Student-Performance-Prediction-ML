270e1706d576bfa739525efb3389b99def2d7adc
ABISHEK ANTONY <abishek8673@gmail.com>
2025-10-13 10:18:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.write("Upload your dataset or enter details to predict student performance.")
 
 
 @st.cache_data
-def load_data():
+def load_data(): 
     return pd.read_csv("AI-Data.csv") 
 
 df = load_data()
