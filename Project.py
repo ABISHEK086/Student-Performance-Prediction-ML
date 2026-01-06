ec0d8da5f725a63f8ee62ddabba85980e0f7e915
ABISHEK ANTONY <abishek8673@gmail.com>
2026-01-06 16:43:14 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -9,7 +9,7 @@ import sklearn.ensemble as es
 import sklearn.metrics as m                    
 import sklearn.linear_model as lm           
 import sklearn.neural_network as nn            
-import numpy as np  
+import numpy as np   
 import warnings as w        
 w.filterwarnings('ignore')    
 data = pd.read_csv("AI-Data.csv")    
