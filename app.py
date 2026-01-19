376c132b032237fd8b782223bec8907a5ad65824
ABISHEK ANTONY <abishek8673@gmail.com>
2026-01-19 17:45:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split 
 from sklearn.metrics import accuracy_score  
 from sklearn.tree import DecisionTreeClassifier    
-from sklearn.ensemble import RandomForestClassifier   
+from sklearn.ensemble import RandomForestClassifier    
 from sklearn.linear_model import LogisticRegression, Perceptron    
 from sklearn.neural_network import MLPClassifier 
 from sklearn.preprocessing import LabelEncoder  
