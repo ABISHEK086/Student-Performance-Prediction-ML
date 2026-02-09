8c08a7284a40f4bae1b997307ae745171fa7f126
ABISHEK ANTONY <abishek8673@gmail.com>
2026-02-09 09:30:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from sklearn.linear_model import LogisticRegression, Perceptron
 from sklearn.neural_network import MLPClassifier  
 from sklearn.preprocessing import LabelEncoder   
   
- 
+  
 st.title("Student Performance Prediction - ML")
 st.write("Upload your dataset or enter details to predict student performance.")
  
